99679ac24ad530976c0aaaf21eaf3fa4c39b644e
whalemare <nanostet@gmail.com>
2018-10-26 18:51:27 +0700

    Save commit
    Experiment : # 1 trainPercent = 60 %
    Experiment : # 2 trainPercent = 70 %
    Experiment : # 3 trainPercent = 80 %
    /Users/whalemare/anaconda2/lib/python2.7/site-packages/sklearn/model_selection/_split.py:2026: FutureWarning: From version 0.21, test_size will always complement train_size unless both are specified.
      FutureWarning)
    [3 3 1 3 0 3 0 2 3 3 3 0 3 3 3 3 3 3 3 3 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2
     2 3 3 3 3 3 0 3 3 3 3 3 0 0 3 3 3 3 3 3 3 3 3 3 3 3 0 3 3 3 3 3 3 3 3 3 3
     3 3 3 2 3 3 2 3 3 3 3 3 3 3 3 3 3 3 0 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
     0 3 3 3 3 3 0 3 3 3 3 3 2 3 3 3 3 3 3 3 3 0 1 3 3 4 3 3 3 3 3 3 2 3 3 3 3
     3 3 3 0 3 0 3 3 3 3 3 3 3 3 3 3 2 3 3 3 3 0 0 3 2 3 3 3 4 3 0 3 2 3 3 3 3
     3 3 3 3 3 3 2 2]
    TOTAL: 193
    guessed: 193
    accuracy: 1.0

--- Main.py
+++ Main.py
@@ -1,58 +1,63 @@
 import StringIO
+import os
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
 import pydotplus
 
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
-def makePdf(percent, classifier):
+def make_pdf(percent, m_classifier):
     dot_data = StringIO.StringIO()
-    tree.export_graphviz(classifier, out_file=dot_data)
+    tree.export_graphviz(m_classifier, out_file=dot_data)
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
     graph.write_png("graph" + str(percent) + ".png")
-
 
 experiments = [
     [1, 60, 40],
     [2, 70, 30],
-    [3, 80, 20],
-    [4, 90, 10]
+    [3, 80, 20]
 ]
 
 for experiment in experiments:
     trainPercent = experiment[1]
 
-    print "Experiment: #", experiment[0], ", trainPercent =", trainPercent, "%"
-    data = []
+    print "Experiment : #", experiment[0], "trainPercent =", trainPercent, "%"
 
-    for line in open('tae.data', 'r'):
-        line = line.split(",")
-        line = map(int, line)
-        data.append(line)
+imported_data = []
 
-    allData = np.array(data)
+for line in open('tae.data', 'r'):
+    line = line.split(",")
 
-    x = allData[:, :4]
-    y = allData[:, 5]
+    newline = []
+    for item in line:
+        if item == "?":
+            newline.append(0)
+        else:
+            newline.append(item)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=trainPercent / 100.0)
+    line = map(int, newline)
+    imported_data.append(line)
 
-    # Train
+all_data = np.array(imported_data)
 
-    classifier = DecisionTreeClassifier(max_depth=5).fit(x_train, y_train)
+X = all_data[:, :5]
+y = all_data[:, 4]
 
-    y_predicted = classifier.predict(x_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=trainPercent / 100.0)
+clf = DecisionTreeClassifier().fit(X_train, y_train)
+y_predicted = clf.predict(X_test)
 
-    guessed_num = 0
+print y_predicted
+guessed_num = 0
 
-    for i in range(0, len(y_predicted)):
-        if y_predicted[i] == y_test[i]:
-            guessed_num += 1
+for ind in range(0, len(y_predicted)):
+    if y_predicted[ind] == y_test[ind]:
+        guessed_num += 1
 
-    print "Total:", len(y_test)
-    print "Guessed:", guessed_num
-    print "Percent:", 100.0 * guessed_num / len(y_test)
-    print ""
-    makePdf(trainPercent, classifier)
+print "TOTAL:", len(y_test)
+print "guessed:", guessed_num
+print "accuracy:", 1.0 * guessed_num / len(y_test)
+print ""
+make_pdf(trainPercent, clf)
